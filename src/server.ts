69bd7df38d94a9fdf6827d220ceb45d4182df303
Pedro Brasca <pedro.brasca@gmail.com>
2024-08-31 14:03:39 -0300

    chore: Add initial project configuration files and dependencies

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import { app } from './app'
+import { env } from './env'
+
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('🚀 Server running on http://localhost:3333')
+  })
+  .catch((err) => {
+    console.error(err)
+  })
