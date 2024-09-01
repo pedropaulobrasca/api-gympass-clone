b9c9632f66f21f0cf0be6f7ceeedee5c8cb86644
Pedro Brasca <pedro.brasca@gmail.com>
2024-08-31 22:03:01 -0300

    feat: Refactor user registration to use registerService
    This commit refactors the user registration endpoint to use the newly created registerService function from the services/register.ts file. The registerService function handles the logic of hashing the password, checking for existing users with the same email, and creating a new user in the database using Prisma. This change improves code organization and separation of concerns.

---
+++ src/services/register.ts
@@ -0,0 +1,30 @@
+import { prisma } from "@/lib/prisma"
+import { hash } from "bcryptjs"
+
+interface RegisterServiceProps {
+  name: string
+  email: string
+  password: string
+}
+
+export async function registerService({name, email, password}: RegisterServiceProps) {
+  const password_hash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    throw new Error('User with same email already exists')
+  }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash,
+    },
+  })
+}
