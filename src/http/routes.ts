f85ee046e6dada3b604287ad857a3bc30b5d5bee
Pedro Brasca <pedropaulobrasca@gmail.com>
2024-09-24 23:36:19 -0300

    ✨ feat: adição de suporte ao fastify-jwt com nova tipagem e importação no app.ts

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,13 @@
 import { FastifyInstance } from 'fastify'
 
 import { authenticate } from './controllers/authenticate'
+import { profile } from './controllers/profile'
 import { registerUser } from './controllers/register-user'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', registerUser)
   app.post('/sessions', authenticate)
+
+  // Authenticated
+  app.get('/me', profile)
 }
