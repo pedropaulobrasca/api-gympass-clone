414f844323d8e88f83e8248034d8822d7dc14669
Pedro Brasca <pedropaulobrasca@gmail.com>
2024-09-24 22:51:47 -0300

    ✨ feat: adicionar repositório Prisma para academias

--- src/repositories/prisma/prisma-check-ins-repository.ts
+++ src/repositories/prisma/prisma-check-ins-repository.ts
@@ -6,8 +6,6 @@ import { prisma } from '@/lib/prisma'
 import { CheckInsRepository } from '../check-ins-repository'
 
 export class PrismaCheckInsRepository implements CheckInsRepository {
-  public items: CheckIn[] = []
-
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = await prisma.checkIn.create({ data })
 
