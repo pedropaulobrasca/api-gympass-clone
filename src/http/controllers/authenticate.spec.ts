89c43e33ddcd8abc04a07c0aab60d1bde773f89c
Pedro Brasca <pedropaulobrasca@gmail.com>
2024-09-29 18:07:19 -0300

    🚨 test: adicionar testes e2e para autenticação e perfil

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,3 +1,32 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('OK', () => {})
+import { app } from '@/app'
+
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to authenticate', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'jonhdoe@example.com',
+      password: '123asd',
+    })
+
+    const response = await request(app.server).post('/sessions').send({
+      email: 'jonhdoe@example.com',
+      password: '123asd',
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
+})
--- src/http/controllers/profile.spec.ts
+++ src/http/controllers/profile.spec.ts
@@ -1,3 +1,41 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('OK', () => {})
+import { app } from '@/app'
+
+describe('Profile (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to get user profile', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'jonhdoe@example.com',
+      password: '123asd',
+    })
+
+    const authResponse = await request(app.server).post('/sessions').send({
+      email: 'jonhdoe@example.com',
+      password: '123asd',
+    })
+
+    const { token } = authResponse.body
+
+    const profileResponse = await request(app.server)
+      .get('/me')
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(profileResponse.statusCode).toEqual(200)
+    expect(profileResponse.body.user).toEqual(
+      expect.objectContaining({
+        email: 'jonhdoe@example.com',
+      }),
+    )
+  })
+})
